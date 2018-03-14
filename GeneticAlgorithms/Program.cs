1ccf79c2cfdca2a252d00b099bc4c5635d20835f
gstavr <gstavr1988@gmail.com>
2018-03-14 13:55:10 +0200

    Continue

--- GeneticAlgorithms/Program.cs
+++ GeneticAlgorithms/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace GeneticAlgorithms
 {
@@ -6,7 +8,23 @@ namespace GeneticAlgorithms
     {
         static void Main(string[] args)
         {
+
+            string constring = "Data Source=DEV-STAVROU\\SQLEXPRESS;Initial Catalog=GeneticAlgorithms;Integrated Security=True";
             Console.WriteLine("Hello World!");
+
+            DataTable dt = new DataTable();
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Test WHERE NAME = 'Test'", con);
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = cmd;
+                adapter.SelectCommand.CommandTimeout = 300;
+                con.Open();
+                adapter.Fill(dt);
+                con.Close();
+            }
+            
         }
     }
 }
